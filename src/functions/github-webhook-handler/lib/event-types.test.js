6e5f1cec40473f712e6f146eabddc9c3120a6eb2
toricls <toriclstr@gmail.com>
2017-08-28 01:43:11 +0900

    Make test fixture easier to read

--- src/functions/github-webhook-handler/lib/event-types.test.js
+++ src/functions/github-webhook-handler/lib/event-types.test.js
@@ -2,18 +2,21 @@
 
 let assert = require('assert')
 
-let pushedEvent = JSON.parse(require('../../../../test/fixtures/pushed').Records[0].Sns.Message)
-let prCreatedEvent = JSON.parse(require('../../../../test/fixtures/pr-created').Records[0].Sns.Message)
+let pushedEvent = require('../../../../test/fixtures/pushed')
+let prCreatedEvent = require('../../../../test/fixtures/pr-created')
 
+let snsnize = require('./snsnizer').snsnize
 let ghEventType = require('./event-types').ghEventType
 
 describe('event-types', function() {
     describe('ghEventType', function() {
         it('should return "push" when the event is pushed event', function() {
-            assert.equal('push', ghEventType(pushedEvent))
+            const message = JSON.parse(snsnize(pushedEvent).Records[0].Sns.Message)
+            assert.equal('push', ghEventType(message))
         })
         it('should return "pr" when the event is pr-created event', function() {
-            assert.equal('pr', ghEventType(prCreatedEvent))
+            const message = JSON.parse(snsnize(prCreatedEvent).Records[0].Sns.Message)
+            assert.equal('pr', ghEventType(message))
         })
     })
 })
