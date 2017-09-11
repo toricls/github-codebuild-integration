456e0eec40516353df7feb2a914fd46cba2ad83e
toricls <toriclstr@gmail.com>
2017-09-12 02:31:23 +0900

    Invoke CodeBuild from webhook handler Lambda function

--- src/functions/github-webhook-handler/index.js
+++ src/functions/github-webhook-handler/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const AWS = require('aws-sdk'),
-    stepfunctions = new AWS.StepFunctions()
+    codebuild = new AWS.CodeBuild()
 const GitHubApi = require('github'),
     github = new GitHubApi({version: '3.0.0'})
 const ghUrl = require('parse-github-url'),
@@ -26,39 +26,45 @@ exports.handler = (event, context, callback) => {
         callback()
     }
 
-    // Create GitHub pending status
+    // Execute CodeBuild Project
     const sha = ghEvent.pull_request ? ghEvent.pull_request.head.sha : ghEvent.head_commit.id
-    const p = new Promise((resolve) => {
-        github.repos.createStatus({
-            owner: repo.owner,
-            repo: repo.name,
-            sha: sha,
-            state: 'pending',
-            target_url: `https://${region}.console.aws.amazon.com/codebuild/home?region=${region}#/builds`,
-            context: 'codebuild',
-            description: 'AWS CodeBuild is preparing your tests...'
-        }).then((data) => {
-            console.log(data)
-            resolve('status created')
-        }).catch((err) => {
-            console.log(err)
-            resolve('status creation failed')
-        })
-    })
-
-    // Execute Step Functions
-    p.then(() => {
-        const params = {
-            stateMachineArn: process.env.STEP_FUNCTIONS_ARN,
-            input: JSON.stringify(ghEvent)
-        }
-        stepfunctions.startExecution(params, function(err, data) {
-            if (err) {
+    const params = {
+        projectName: process.env.CODEBUILD_PROJECT_NAME,
+        sourceVersion: sha
+    }
+    codebuild.startBuild(params, (err, data) => {
+        // Create GitHub status
+        if (err) {
+            // If CodeBuild couldn't start a build, save GitHub status as 'error'
+            console.log(err, err.stack)
+            github.repos.createStatus({
+                owner: repo.owner,
+                repo: repo.name,
+                sha: sha,
+                state: 'error',
+                target_url: `https://${region}.console.aws.amazon.com/codebuild/home?region=${region}#/builds`,
+                context: 'codebuild',
+                description: 'AWS CodeBuild has failed to start your tests'
+            }).then(() => {
+                callback(err)
+            }).catch((err) => {
+                callback(err)
+            })
+        } else {
+            github.repos.createStatus({
+                owner: repo.owner,
+                repo: repo.name,
+                sha: sha,
+                state: 'pending',
+                target_url: `https://${region}.console.aws.amazon.com/codebuild/home?region=${region}#/builds/${data.build.id}/view/new`,
+                context: 'codebuild',
+                description: 'AWS CodeBuild is running your tests'
+            }).then((data) => {
+                callback(null, data)
+            }).catch((err) => {
                 console.log(err, err.stack)
                 callback(err)
-            } else {
-                callback(null, data)
-            }
-        })
+            })
+        }
     })
 }
